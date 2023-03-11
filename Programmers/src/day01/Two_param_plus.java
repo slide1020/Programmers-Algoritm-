b88fddfb9b7afaed408b9521375b82e100f6d35a
slide1020 <slide1020@naver.com>
2023-03-11 20:37:28 +0900

    230311 sol1 success

--- Programmers/src/day01/Two_param_plus.java
+++ Programmers/src/day01/Two_param_plus.java
@@ -1,23 +1,15 @@
 /*
  * https://school.programmers.co.kr/learn/courses/30/lessons/120802?language=java
  * 230307..Fail
+ * 230311 success
  */
 package day01;
 
 import java.util.Scanner;
 
 class Solution {
-    public int solution(int num1, int num2) {
-        int answer = -1;
-        
-        answer = num1 + num2;
-        return answer;
-    }
-}
-
-public class Two_param_plus {
-	
-	public static void main(String[] args){
+    
+    public static void main(String[] args){
 		int num1 = 0;
 	    int num2 = 0;
 		
@@ -29,13 +21,17 @@ public class Two_param_plus {
 	    System.out.println("num 2 : ");
 	    num2 = sc.nextInt();
 	    
-	    Solution sol = new Solution();
-	    
-	    int result = sol.solution(num1, num2);
+	    int result = solution(num1, num2);
 	    System.out.println("result : " + result);
 	       
 	}
-	
+    
+	public static int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = num1 + num2;
+        return answer;
+    }
 }
 
 
