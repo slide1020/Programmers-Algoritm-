2272980071188c5cb47d6de615a74b6190221fea
slide1020 <slide1020@naver.com>
2023-03-11 20:51:49 +0900

    230311 Make solution

---
+++ Programmers/src/day2_hash_poketmon/solution.java
@@ -0,0 +1,11 @@
+/*
+ * 프로그래머스 > 코테연습 > 코테고득점kit > 해시
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1845
+ * 해시 포켓몬
+ */
+
+package day2_hash_poketmon;
+
+public class solution {
+
+}
